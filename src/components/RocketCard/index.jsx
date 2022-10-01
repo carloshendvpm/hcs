320c36af0dd1666100fc3059f6cf90d533feef39
Carlos Henrique <hencarlosdv@gmail.com>
2022-10-01 17:28:26 -0300

    feat: adding the main button

--- src/components/RocketCard/index.jsx
+++ src/components/RocketCard/index.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Rocket from '../../assets/img/rocket.png'
+import { motion } from "framer-motion"
 import './style.css'
 
 export const RocketCard = (props) => {
+  const [move, setMove] = useState(false);
   return (
-    <div className='Card' onClick={() => console.log("Clicou")}>
-        <img src={Rocket}/>
+    <div  className='Card' onClick={() => console.log("Clicou")}>
+        <motion.img 
+        animate={{ y: move ? 1000 : -1000 }} 
+        transition={{ type:'tween', duration: 5}}
+        onClick={ () => {
+          setMove(!move)
+        }}
+        src={Rocket}/>
         <p>{props.text}</p>
     </div>
   )
