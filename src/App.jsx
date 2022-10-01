d6164ac33239cee076fca2b2d4ecca856e428513
Carlos Henrique <hencarlosdv@gmail.com>
2022-10-01 16:17:06 -0300

    feat: add questions data

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function App() {
   return (
     <div>
-      <h1>Here comes the sun</h1>
+      <h1>You are about to embark on an unforgettable galactic adventure!<br/> Choose your flight and let’s go!</h1>
     </div>
   )
 }
